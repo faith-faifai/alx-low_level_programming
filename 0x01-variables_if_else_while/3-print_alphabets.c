a445d2b6d1f723e8e7e27700c771cfefdc4bc5f3
faith-faifai <tusiimebaingana@gmial.com>
2022-03-11 20:33:30 +0300

    Lowercase and Uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+
+int main(void)
+{
+        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
+
