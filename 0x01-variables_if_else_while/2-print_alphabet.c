ad473d21198a08c02accbe0491d716d65417b0f1
faith-faifai <tusiimebaingana@gmial.com>
2022-03-11 20:16:58 +0300

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints the alphet in lowercase
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i=0;i < 26; i++)
+	{
+		putchar(alp[1]);
+	}
+	putcahr('\n');
+	return (0);
+}
