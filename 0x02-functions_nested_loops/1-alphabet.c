76b01aeccaa8f94bd7dba8f86856243a5b7792e4
faith-faifai <tusiimebaingana@gmial.com>
2022-03-15 21:45:05 +0300

    ALPHABET

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 
-	void print_alphabet(void)
+void print_alphabet(void)
+{
 	int i;
 
 	for (i = 97; i <= 122; i++)
